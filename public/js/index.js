054dc4f87e08e7ba9ae28da80dcc89fb4176247e
Samoel33 <68919054+Samoel33@users.noreply.github.com>
2022-01-25 18:51:44 +0200

    service and contact changes

--- public/js/index.js
+++ public/js/index.js
@@ -7,43 +7,58 @@ const navList = document.querySelectorAll('ul li a');
 navList.forEach(link => {
     if (link.href.includes(`${activeLink}`)) link.classList.add('active')
 });
-const navHeight = nav.getBoundingClientRect().height;
-const stickyNav = function(entries) {
-    const [entry] = entries;
-    console.log(entries);
-    if (!entry.isIntersecting) nav.classList.add('sticky');
-    else nav.classList.remove('sticky');
-}
-const headerObserver = new IntersectionObserver(
-    stickyNav, {
-        root: null,
-        threshold: 0.2,
-        //rootMargin: `-${navHeight}px`
+if (landingPageServices) {
+    const navHeight = nav.getBoundingClientRect().height;
+    const stickyNav = function(entries) {
+        const [entry] = entries;
+        console.log(entries);
+        if (!entry.isIntersecting) nav.classList.add('sticky');
+        else nav.classList.remove('sticky');
     }
-)
-headerObserver.observe(landingPageServices);
+    const headerObserver = new IntersectionObserver(
+        stickyNav, {
+            root: null,
+            threshold: 0.2,
+            //rootMargin: `-${navHeight}px`
+        }
+    )
+    headerObserver.observe(landingPageServices);
+}
 
 //IMAGE VISIBLE ON SCROLL USING INTERSECTION oBSERVER
 const main_container = document.querySelectorAll('.main-container-1');
 const images = document.querySelectorAll('.cover-image-1');
-console.log(main_container);
-const imageVisibility = function(entries, observer) {
-    const [entry] = entries;
-    console.log(entry);
-    if (!entry.isIntersecting) return;
-    else {
-        entry.target.classList.remove("hide-section");
-        entry.target.classList.add('animate-in');
-        observer.unobserve(entry.target);
+if (main_container) {
+    const imageVisibility = function(entries, observer) {
+        const [entry] = entries;
+        console.log(entry);
+        if (!entry.isIntersecting) return;
+        else {
+            entry.target.classList.remove("hide-section");
+            entry.target.classList.add('animate-in');
+            observer.unobserve(entry.target);
+        }
     }
+    const imageVisible = new IntersectionObserver(
+        imageVisibility, {
+            root: null,
+            threshold: 0.5,
+        }
+    );
+    images.forEach(image => {
+        imageVisible.observe(image);
+        image.classList.add("hide-section");
+    })
 }
-const imageVisible = new IntersectionObserver(
-    imageVisibility, {
-        root: null,
-        threshold: 0.5,
-    }
-);
-images.forEach(image => {
-    imageVisible.observe(image);
-    image.classList.add("hide-section");
+
+//BURGER ANIMATION
+const navigationBarList = document.querySelector('.navigation-bar-lists');
+const burger = document.querySelector('.burger');
+const lines = document.querySelector('.line');
+
+burger.addEventListener('click', (e) => {
+    e.preventDefault(e);
+    burger.classList.toggle('active-burger');
+    navigationBarList.classList.toggle('active-Nav');
+    lines.classList.add('rotateLine');
 })
